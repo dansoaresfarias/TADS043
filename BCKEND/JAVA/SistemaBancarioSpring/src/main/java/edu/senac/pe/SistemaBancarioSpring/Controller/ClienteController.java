1ad72cfc646db71275e8d742f0c537470d2597be
Danilo Farias <dansoaresfarias@gmail.com>
2025-11-25 20:38:28 -0300

    API Web TS

--- BCKEND/JAVA/SistemaBancarioSpring/src/main/java/edu/senac/pe/SistemaBancarioSpring/Controller/ClienteController.java
+++ BCKEND/JAVA/SistemaBancarioSpring/src/main/java/edu/senac/pe/SistemaBancarioSpring/Controller/ClienteController.java
@@ -3,6 +3,7 @@ package edu.senac.pe.SistemaBancarioSpring.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -32,6 +33,18 @@ public class ClienteController {
 		Iterable<Cliente> clientes = cr.findAll();
 		mv.addObject("clientes", clientes);
 		return mv;
+	}
+	
+	@GetMapping("/excluirCliente/{cpf}")
+	public String excluirCliente(@PathVariable String cpf) {
+		cr.deleteById(cpf);
+		return "redirect:/listarClientes";
+	}
+	
+	@PostMapping("/detalharCliente/{cpf}")
+	public String atualizarCliente(Cliente cliente) {
+		cr.save(cliente);
+		return "redirect:/";
 	}
 	
 }
---
+++ BCKEND/TypeScript/BancoFinanceiroNodeTypeScript/src/repositories/ClienteRepository.ts
@@ -0,0 +1,60 @@
+import { AppDataSource } from "../db/data-source";
+import { Cliente } from "../models/Cliente";
+
+class ClienteRepository {
+    clienteRepository = AppDataSource.getRepository(Cliente);
+
+    async createCliente(cliente: Cliente): Promise<Cliente> {
+        try {
+            return await this.clienteRepository.save(cliente);
+        } catch (error) {
+            throw new Error(`Erro ao criar cliente: ${error}`);
+        }
+    }
+
+    async getAllClientes(): Promise<Array<Cliente>> {
+        try {
+            return await this.clienteRepository.find();
+        } catch (error) {
+            throw new Error(`Erro ao buscar clientes: ${error}`);
+        }
+    }
+
+    async getClienteByCPF(cpf: string): Promise<Cliente | null> {
+        try {
+            return await this.clienteRepository.findOneBy({ cpf: cpf });
+        } catch (error) {
+            throw new Error(`Erro ao buscar cliente por CPF: ${error}`);
+        }
+    }
+
+    async updateCliente(cliente: Cliente) {
+        const { cpf, nome, dataNascimento, rg, email, telefone } = cliente;
+        try {
+            return await this.clienteRepository.save(cliente);
+        } catch (error) {
+            throw new Error(`Erro ao atualizar cliente: ${error}`);
+        }
+    }
+
+    async deleteCliente(cpf: string) {
+        try {
+            await this.clienteRepository.delete(cpf);
+        } catch (error) {
+            throw new Error(`Erro ao deletar cliente: ${error}`);
+        }
+    }
+
+    async deleteAll(): Promise<number> {
+        try {
+            let num = this.clienteRepository.query("select count(cpf) from clientes;");
+            this.clienteRepository.query("delete from clientes;");
+            return num;
+        } catch (error) {
+            throw new Error("Falha ao deletar todos os clientes!");
+        }
+    }
+
+}
+
+export default new ClienteRepository();
